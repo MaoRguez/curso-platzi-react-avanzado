066c71f9eb464b28de2042a9bce01f1bd34df811
Maorguez <avila.mauro@gmail.com>
2020-08-03 11:23:38 -0500

    nombre en package.json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import { App } from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
